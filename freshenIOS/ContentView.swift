1b0e8913afd7836e493eb8dc18ec92dce2526017
Klay Clarke <clarkeklay@gmail.com>
2023-06-21 18:32:33 -0400

    First commit

---
+++ freshenIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  freshenIOS
+//
+//  Created by Klay Anthony Clarke on 6/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ freshenIOS/freshenIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  freshenIOSApp.swift
+//  freshenIOS
+//
+//  Created by Klay Anthony Clarke on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct freshenIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
